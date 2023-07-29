1dadceef41ca8f9c01c05bf16c3a6595abd4c5ec
injetys <68036499+injetys@users.noreply.github.com>
2023-07-29 00:09:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,64 @@
 // Function to parse CSV data using D3
 function parseCSVData(csvData) {
-    const rows = d3.csvParseRows(csvData);
+    const rows = d3.csvParse(csvData);
     return rows;
 }
 
-// Function to display the row count on the HTML page
-function displayRowCount(rowCount) {
-    const rowCountElement = d3.select("#row-count");
-    rowCountElement.text(`Row Count: ${rowCount}`);
+// Function to create and display the bar chart
+function displayBarChart(data) {
+    const counts = {};
+    data.forEach(row => {
+        const nationality = row.nationality_name;
+        counts[nationality] = (counts[nationality] || 0) + 1;
+    });
+
+    const chartContainer = d3.select("#chart");
+    const chartWidth = 500;
+    const chartHeight = 300;
+    const barPadding = 5;
+
+    const xScale = d3.scaleBand()
+        .domain(Object.keys(counts))
+        .range([0, chartWidth])
+        .padding(0.1);
+
+    const yScale = d3.scaleLinear()
+        .domain([0, d3.max(Object.values(counts))])
+        .range([0, chartHeight]);
+
+    const svg = chartContainer.append("svg")
+        .attr("width", chartWidth)
+        .attr("height", chartHeight);
+
+    const bars = svg.selectAll("rect")
+        .data(Object.entries(counts))
+        .enter()
+        .append("rect")
+        .attr("x", d => xScale(d[0]))
+        .attr("y", d => chartHeight - yScale(d[1]))
+        .attr("width", xScale.bandwidth())
+        .attr("height", d => yScale(d[1]))
+        .attr("fill", "steelblue");
+
+    // Adding labels to the bars
+    svg.selectAll(".bar-label")
+        .data(Object.entries(counts))
+        .enter()
+        .append("text")
+        .attr("class", "bar-label")
+        .attr("x", d => xScale(d[0]) + xScale.bandwidth() / 2)
+        .attr("y", d => chartHeight - yScale(d[1]) - 5)
+        .attr("text-anchor", "middle")
+        .text(d => d[1]);
 }
 
-// Function to display the data table on the HTML page
-function displayDataTable(csvData) {
-    const data = parseCSVData(csvData);
-    const table = d3.select("#data-table");
-
-    // Creating the table headers
-    const headers = data[0];
-    const headerRow = table.append("tr");
-    headerRow.selectAll("th")
-        .data(headers)
-        .enter()
-        .append("th")
-        .text(header => header);
-
-    // Creating the table rows
-    const rows = table.selectAll("tr.data-row")
-        .data(data.slice(1)) // Skip the first row (header)
-        .enter()
-        .append("tr")
-        .attr("class", "data-row");
-
-    // Adding cells to the rows
-    rows.selectAll("td")
-        .data(row => row)
-        .enter()
-        .append("td")
-        .text(cell => cell);
-}
-
-// Main function to fetch data and display on the page
+// Main function to fetch data and display the bar chart
 function main() {
     const csvFilePath = 'female_players_legacy.csv';
     d3.text(csvFilePath)
         .then(csvData => {
-            const rowCount = parseCSVData(csvData).length;
-            displayRowCount(rowCount);
-            displayDataTable(csvData);
+            const data = parseCSVData(csvData);
+            displayBarChart(data);
         })
         .catch(error => console.error("Error fetching data:", error));
 }
