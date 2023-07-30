000a2ca0225e70d35fa98a31a20d3f4d79ececa2
injetys <68036499+injetys@users.noreply.github.com>
2023-07-29 19:50:21 -0700

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,8 @@ function countRows(csvData) {
 // ... (previous code)
 
 // Function to create and display the bar chart for Scene 1
-function displayBarChartScene1(data) {
+// Function to create and display the pie chart for Scene 1
+function displayPieChartScene1(data) {
     const counts = {};
     data.forEach(row => {
         const nationality = row.nationality_name;
@@ -25,42 +26,42 @@ function displayBarChartScene1(data) {
     const chartContainer = d3.select("#chart1");
     chartContainer.html(""); // Clear previous content
 
-    const chartWidth = 500;
+    const chartWidth = 300;
     const chartHeight = 300;
-    const margin = { top: 20, right: 20, bottom: 40, left: 60 };
-    const barPadding = 5;
-
-    const xScale = d3.scaleBand()
-        .domain(Object.keys(counts))
-        .range([margin.left, chartWidth - margin.right])
-        .padding(0.1);
-
-    const yScale = d3.scaleLinear()
-        .domain([0, d3.max(Object.values(counts))])
-        .range([chartHeight - margin.bottom, margin.top]);
+    const radius = Math.min(chartWidth, chartHeight) / 2;
 
     const svg = chartContainer.append("svg")
         .attr("width", chartWidth)
-        .attr("height", chartHeight);
-    
+        .attr("height", chartHeight)
+        .append("g")
+        .attr("transform", `translate(${chartWidth / 2}, ${chartHeight / 2})`);
 
-    // Adding bars
-    const bars = svg.selectAll("rect")
-        .data(Object.entries(counts))
+    const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+    const pie = d3.pie()
+        .value(d => d.value)
+        .sort(null);
+
+    const dataForPie = pie(d3.entries(counts));
+
+    const arc = d3.arc()
+        .innerRadius(0)
+        .outerRadius(radius);
+
+    // Adding pie slices
+    const slices = svg.selectAll("path")
+        .data(dataForPie)
         .enter()
-        .append("rect")
-        .attr("x", d => xScale(d[0]))
-        .attr("y", d => yScale(d[1]))
-        .attr("width", xScale.bandwidth())
-        .attr("height", d => chartHeight - margin.bottom - yScale(d[1]))
-        .attr("fill", "steelblue")
+        .append("path")
+        .attr("d", arc)
+        .attr("fill", (d, i) => colorScale(i))
         .on("mouseover", function (event, d) {
             // Tooltip for Scene 1: Show nationality_name on hover
             const tooltip = d3.select("#tooltip");
             tooltip.style("display", "inline")
                 .style("left", (event.pageX + 10) + "px")
                 .style("top", (event.pageY - 25) + "px")
-                .text(d[0]);
+                .text(d.data.key);
         })
         .on("mouseout", function () {
             const tooltip = d3.select("#tooltip");
@@ -68,16 +69,10 @@ function displayBarChartScene1(data) {
         })
         .on("click", function (event, d) {
             // Trigger drill down to Scene 2 with selected nationality
-            showScene2(d[0]);
+            showScene2(d.data.key);
         });
-
-    // Adding y-axis
-    const yAxis = d3.axisLeft(yScale);
-    svg.append("g")
-        .attr("class", "y-axis")
-        .attr("transform", `translate(${margin.left}, 0)`)
-        .call(yAxis);
 }
+
 
 // Function to create and display the bar chart for Scene 2
 function displayBarChartScene2(data, selectedNationality) {
@@ -154,7 +149,7 @@ function showScene1() {
         .then(csvData => {
             const data = parseCSVData(csvData);
             console.log(data); // Add this line to check the parsed data
-            displayBarChartScene1(data);
+            displayPieChartScene1(data); // Change this line to display the pie chart
         })
         .catch(error => console.error("Error fetching data:", error));
 }
