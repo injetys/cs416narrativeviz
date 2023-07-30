a15d079cce38ff8b3a14f6e92bfaf1aceb85f342
injetys <68036499+injetys@users.noreply.github.com>
2023-07-29 18:14:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function displayBarChartScene1(data) {
         counts[nationality] = (counts[nationality] || 0) + 1;
     });
 
-    const chartContainer = d3.select("#chart1");
+    const chartContainer = d3.select("#chart");
     chartContainer.html(""); // Clear previous content
 
     const chartWidth = 500;
@@ -48,8 +48,11 @@ function displayBarChartScene1(data) {
         .on("mouseout", function () {
             const tooltip = d3.select("#tooltip");
             tooltip.style("display", "none");
+        })
+        .on("click", function (event, d) {
+            showScene2(d[0]);
         });
-        
+
     // Adding y-axis
     const yAxis = d3.axisLeft(yScale);
     svg.append("g")
@@ -58,55 +61,59 @@ function displayBarChartScene1(data) {
         .call(yAxis);
 }
 
-// Function to create and display the scatter plot for Scene 2
-function displayScatterPlotScene2(data) {
-    const chartContainer = d3.select("#chart2");
+// ... (rest of the code for Scene 1)
+// Function to create and display the bar chart for Scene 2
+function displayBarChartScene2(data, selectedNationality) {
+    const filteredData = data.filter(row => row.nationality_name === selectedNationality);
+    const counts = {};
+    filteredData.forEach(row => {
+        const league = row.league;
+        counts[league] = (counts[league] || 0) + 1;
+    });
+
+    const chartContainer = d3.select("#chart");
     chartContainer.html(""); // Clear previous content
 
     const chartWidth = 500;
     const chartHeight = 300;
     const margin = { top: 20, right: 20, bottom: 40, left: 60 };
+    const barPadding = 5;
 
-    const xScale = d3.scaleLinear()
-        .domain(d3.extent(data, d => +d.age))
-        .range([margin.left, chartWidth - margin.right]);
+    const xScale = d3.scaleBand()
+        .domain(Object.keys(counts))
+        .range([margin.left, chartWidth - margin.right])
+        .padding(0.1);
 
     const yScale = d3.scaleLinear()
-        .domain(d3.extent(data, d => +d.overall))
+        .domain([0, d3.max(Object.values(counts))])
         .range([chartHeight - margin.bottom, margin.top]);
 
     const svg = chartContainer.append("svg")
         .attr("width", chartWidth)
         .attr("height", chartHeight);
 
-    // Adding circles
-    const circles = svg.selectAll("circle")
-        .data(data)
+    // Adding bars
+    const bars = svg.selectAll("rect")
+        .data(Object.entries(counts))
         .enter()
-        .append("circle")
-        .attr("cx", d => xScale(+d.age))
-        .attr("cy", d => yScale(+d.overall))
-        .attr("r", 5)
+        .append("rect")
+        .attr("x", d => xScale(d[0]))
+        .attr("y", d => yScale(d[1]))
+        .attr("width", xScale.bandwidth())
+        .attr("height", d => chartHeight - margin.bottom - yScale(d[1]))
         .attr("fill", "steelblue")
-        .attr("title", d => `${d.age} years, Overall: ${d.overall}`)
+        .attr("title", d => d[0]) // Add tooltip for league name
         .on("mouseover", function (event, d) {
             const tooltip = d3.select("#tooltip");
             tooltip.style("display", "inline")
                 .style("left", (event.pageX + 10) + "px")
                 .style("top", (event.pageY - 25) + "px")
-                .text(`${d.age} years, Overall: ${d.overall}`);
+                .text(d[0]);
         })
         .on("mouseout", function () {
             const tooltip = d3.select("#tooltip");
             tooltip.style("display", "none");
         });
-
-    // Adding x-axis
-    const xAxis = d3.axisBottom(xScale);
-    svg.append("g")
-        .attr("class", "x-axis")
-        .attr("transform", `translate(0, ${chartHeight - margin.bottom})`)
-        .call(xAxis);
 
     // Adding y-axis
     const yAxis = d3.axisLeft(yScale);
@@ -115,6 +122,9 @@ function displayScatterPlotScene2(data) {
         .attr("transform", `translate(${margin.left}, 0)`)
         .call(yAxis);
 }
+
+// ... (rest of the code for Scene 2)
+// ... (rest of the code)
 
 // Function to trigger Scene 1 (Overview)
 function showScene1() {
@@ -127,17 +137,18 @@ function showScene1() {
         .catch(error => console.error("Error fetching data:", error));
 }
 
-// Function to trigger Scene 2 (Scatter Plot)
-function showScene2() {
+// ... (rest of the code)
+
+// Function to trigger Scene 2 (Drill Down)
+function showScene2(selectedNationality) {
     const csvFilePath = 'female_players_legacy.csv';
     d3.text(csvFilePath)
         .then(csvData => {
             const data = parseCSVData(csvData);
-            displayScatterPlotScene2(data);
+            displayBarChartScene2(data, selectedNationality);
         })
         .catch(error => console.error("Error fetching data:", error));
 }
 
-// Call the main functions when the script is loaded
+// Call the main function when the script is loaded
 showScene1();
-showScene2();
